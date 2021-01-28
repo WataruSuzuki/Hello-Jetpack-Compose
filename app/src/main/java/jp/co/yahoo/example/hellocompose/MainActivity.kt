3d51650b1366d02fc05290ae469891d9790d9103
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-01-28 22:10:00 +0900

    Using column

--- app/src/main/java/jp/co/yahoo/example/hellocompose/MainActivity.kt
+++ app/src/main/java/jp/co/yahoo/example/hellocompose/MainActivity.kt
@@ -3,6 +3,7 @@ package jp.co.yahoo.example.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -21,9 +22,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NewsStory() {
-    Text("A day in Shark Fin Cove")
-    Text("Davenport, California")
-    Text("December 2018")
+    Column {
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
 }
 
 @Preview
