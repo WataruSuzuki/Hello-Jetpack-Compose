88a3f17b000fe70d756bd66aed3b4c93730615dc
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-01-29 22:10:00 +0900

    Using modifier

--- app/src/main/java/jp/co/yahoo/example/hellocompose/MainActivity.kt
+++ app/src/main/java/jp/co/yahoo/example/hellocompose/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import jp.co.yahoo.example.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NewsStory() {
-    Column {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
