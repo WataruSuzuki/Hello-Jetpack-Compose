8c77ec5487e538218c24bb21bc7cf35f55e20fa0
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-02-02 22:10:00 +0900

    Using image by foundation

--- app/src/main/java/jp/co/yahoo/example/hellocompose/MainActivity.kt
+++ app/src/main/java/jp/co/yahoo/example/hellocompose/MainActivity.kt
@@ -3,16 +3,15 @@ package jp.co.yahoo.example.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import jp.co.yahoo.example.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +27,10 @@ fun NewsStory() {
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = null
+        )
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
