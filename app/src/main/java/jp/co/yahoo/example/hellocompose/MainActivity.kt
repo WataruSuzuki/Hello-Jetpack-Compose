f0ce6ec2155ec297f4a9e8e4ef9d6609f7df7b43
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-02-04 22:10:00 +0900

    Added spacer

--- app/src/main/java/jp/co/yahoo/example/hellocompose/MainActivity.kt
+++ app/src/main/java/jp/co/yahoo/example/hellocompose/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -38,6 +35,8 @@ fun NewsStory() {
                 .fillMaxWidth(),
             contentScale = ContentScale.Crop
         )
+        Spacer(Modifier.height(16.dp))
+        
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
