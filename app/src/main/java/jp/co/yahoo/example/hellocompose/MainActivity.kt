7fd9b453ecde97bb2db2a4b433b4454d014b6f8a
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-02-15 22:10:00 +0900

    Applied typography

--- app/src/main/java/jp/co/yahoo/example/hellocompose/MainActivity.kt
+++ app/src/main/java/jp/co/yahoo/example/hellocompose/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun NewsStory() {
     MaterialTheme {
+        val typography = MaterialTheme.typography
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
@@ -42,9 +43,12 @@ fun NewsStory() {
             )
             Spacer(Modifier.height(16.dp))
 
-            Text("A day in Shark Fin Cove")
-            Text("Davenport, California")
-            Text("December 2018")
+            Text("A day in Shark Fin Cove",
+                style = typography.h6)
+            Text("Davenport, California",
+                style = typography.body2)
+            Text("December 2018",
+                style = typography.body2)
         }
     }
 }
